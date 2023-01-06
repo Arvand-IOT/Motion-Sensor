a92037365989775a67dcd5c22949facd471499b7
Arash Hatami <hatamiarash7@gmail.com>
2023-01-06 13:08:40 +0330

    Update env.h

--- env.h
+++ env.h
@@ -1,9 +1,9 @@
 #ifndef ENV_H
 #define ENV_H
 
-extern String wifi_ssid;                    // WiFi SSID
-extern String wifi_password;                // WiFi Password
-extern const String alert_url;              // Alert URL
-extern uint8_t alert_url_fingerprint[20];    // Alert URL SHA1 fingerprint
+extern const char* wifi_ssid;                    // WiFi SSID
+extern const char* wifi_password;                // WiFi Password
+extern const String alert_url;                   // Alert URL
+extern const uint8_t alert_url_fingerprint[20];  // Alert URL SHA1 fingerprint
 
 #endif
